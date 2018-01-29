44b9939e872d578e1a4785fcce547182d9c884ed
mackenzieg <mackenziejamesgoodwin@gmail.com>
2018-01-29 18:44:27 -0500

    Added bullshit comments for the marks

--- project2/src/Resizable_deque.h
+++ project2/src/Resizable_deque.h
@@ -83,6 +83,7 @@ Resizable_deque<Type>::Resizable_deque(Resizable_deque const &deque) :
   write_index(0),
   fifo(new Type[allocated])
 {
+  // Iiterate through other array to copy elements. Use write_index and current write index
   for (int i = deque.read_index; i != deque.write_index; i = (i + 1) % allocated) {
     fifo[write_index++] = deque.fifo[i];
   }
@@ -108,6 +109,8 @@ Resizable_deque<Type>::~Resizable_deque() {
 
 template <typename Type>
 int Resizable_deque<Type>::size() const {
+  // If write_index < read_index then we need to take care of
+  // the circular wrapping
   if (write_index < read_index) {
     return (write_index) + (allocated - read_index);
   }
@@ -138,13 +141,15 @@ Type Resizable_deque<Type>::back() const {
   if (empty()) {
     throw underflow();
   }
-  // Have to subtract one to bring us to the read space end
+  // Have to subtract one to bring us to the read space end. Also have
+  // to take care of wrapping in the array
 	return fifo[write_index == 0 ? (allocated - 1) : write_index - 1];
 }
 
 template <typename Type>
 void Resizable_deque<Type>::swap(Resizable_deque<Type> &deque)
 {
+  //  Do a shallow swap of class variables
   std::swap(init_aloc, deque.init_aloc);
   std::swap(allocated, deque.allocated);
 
@@ -171,6 +176,7 @@ Resizable_deque<Type> &Resizable_deque<Type>::operator=( Resizable_deque<Type> &
 
 template <typename Type>
 void Resizable_deque<Type>::push_front( Type const &obj ) {
+  // Take care of wrapping on the read_index by bringing it to the end of the array - 1
   if (read_index == 0) {
     read_index = allocated - 1;
   } else {
@@ -186,7 +192,7 @@ void Resizable_deque<Type>::push_front( Type const &obj ) {
 template <typename Type>
 void Resizable_deque<Type>::push_back( Type const &obj ) {
   fifo[write_index] = obj;
-
+  // Wrap over the array by using modulus
   write_index = (write_index + 1) % allocated;
 
   check_resize();
@@ -221,6 +227,7 @@ void Resizable_deque<Type>::pop_back() {
 
 template <typename Type>
 void Resizable_deque<Type>::clear() {
+  // If empty this shouldn't do anything
   if (empty()) {
     return;
   }
@@ -242,6 +249,8 @@ void Resizable_deque<Type>::check_resize() {
   if (size() >= (allocated / 2)) {
     // Need to make array larger
     new_allocated = allocated * 2;
+    // If size has gone below or equal to  a 1/4 of allocated resize to a smaller
+    // array
   } else if(size() <= (allocated / 4) && allocated != init_aloc) {
     // Need to make array smaller
     new_allocated = allocated / 4;
@@ -261,6 +270,7 @@ void Resizable_deque<Type>::check_resize() {
 
   int new_index  = 0;
 
+  // Copy over elements to new array
   while (curr_index != write_index) {
     new_fifo[new_index++] = fifo[curr_index];
     
